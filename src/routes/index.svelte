047f67077888dc22c8dfa62fc1f8b1dc87bbf45a
gonca-code <gonca.code@gmail.com>
2022-06-26 22:47:59 -0300

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import ytSubs from '../yt-subs.json';
+
+	const content = [...ytSubs].sort((a, b) => a.snippet.title.localeCompare(b.snippet.title));
+</script>
+
+<ul>
+	{#each content as { snippet }, index}
+		<li>
+			<a target="_blank" href="https://www.youtube.com/channel/{snippet.resourceId.channelId}">
+				{index + 1}: {snippet.title}
+			</a>
+		</li>
+	{/each}
+</ul>
